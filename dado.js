18a9d6bb52ddff75f1fd4f55b91a5c4422fc9345
Kassito <98308251+Kassito@users.noreply.github.com>
2023-01-11 00:01:24 +0100

    adding cookies

--- dado.js
+++ dado.js
@@ -1,4 +1,14 @@
-const boton = document.addEventListener("click", girarDado);
+const boton = document.getElementById("boton");
+const cooki = document.getElementById("cooki");
+const cookiRet = document.getElementById("cookiRet");
+const cookiDel = document.getElementById("cookiDel");
+
+
+boton.addEventListener("click", girarDado);
+cooki.addEventListener("click", cookiland);
+cookiRet.addEventListener("click", cookilandRet);
+cookiDel.addEventListener("click", cookilandDel);
+
 const images = ["dado1.png", "dado2.png", "dado3.png", "dado4.png", "dado5.png", "dado6.png"];
 
 let dice = document.querySelectorAll("img")
@@ -19,4 +29,17 @@ function girarDado(){
         document.getElementById("dadoB").setAttribute("src" , images[dieTwoValue]);
 
     }, 1000);
+}
+
+function cookiland() {
+    document.cookie = "username=John Doe; expires=Thu, 19 Jan 2023 12:00:00 UTC";
+}
+
+function cookilandRet() {
+    let cok = document.cookie;
+    alert(cok);
+}
+
+function cookilandDel() {
+    document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 }
